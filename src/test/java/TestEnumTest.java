8bdab925ad6ad49e438d9ae8d6c209709125ce3c
jamez <hjhng125@nate.com>
2022-12-07 17:32:26 +0900

    enum 에 Functional Interface 필드

--- src/test/java/TestEnumTest.java
+++ src/test/java/TestEnumTest.java
@@ -1,4 +1,13 @@
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
 class TestEnumTest {
-  
+
+  @Test
+  void apply() {
+    String apply = TestEnum.ABC.apply("테스트!");
+
+    assertEquals("abc테스트!", apply);
+  }
 }
